6d155e1711ee9295cf12afec76c48777da376278
yurihead <ylzz1997@outlook.com>
2020-11-01 16:50:01 +0800

    Add auto list_num

--- src/Main.java
+++ src/Main.java
@@ -28,10 +28,14 @@ public class Main {
         CANDIDATE_NUM = LC.get(0).getContent().size();
         ahca = new AHCA(LC,1.0/NODE_NUM+0.05);
         AprioriHashTree aht = new AprioriHashTree();
-        if(!aht.addAll(LC)){
+        while(!aht.addAll(LC)){
+            LIST_NUM++;
+            aht = new AprioriHashTree();
+        }
+        /*if(!aht.addAll(LC)){
             System.out.println("树高度高于候选集的项数！请调整节点数量或内容数组数量以适应更大数量的候选集!");
             return;
-        }
+        }*/
         List<String> TI = fileUtil.readT();
         List<Transaction> LT = fileUtil.toTransaction(TI);
         for(Transaction t:LT){
